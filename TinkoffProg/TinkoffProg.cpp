dea7438aa45a5cfd4e989b8c477fab68fdf8ebae
Sara894 <t48250klo@gmail.com>
2022-01-28 22:38:20 +0300

    Добавьте файлы проекта.

---
+++ TinkoffProg/TinkoffProg.cpp
@@ -0,0 +1,17 @@
+﻿// TinkoffProg.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A, B,n;
+	cin >> A >> B >> n;
+	if (A <= B) cout << "NO" << endl;
+	else 
+	{
+		cout << "YES" << endl;
+	}
+}
+
